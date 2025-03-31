37cd74d652cec9481330e6897554e78fd2cc3e8b
unknown <botoshkin.b@gmail.com>
2025-03-31 14:42:09 +0300

    Занятие 1: Основы JavaScript

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,9 +10,13 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  return (
+    name !== null &&          
+    typeof name === 'string' && 
+    name.trim().length >= 4 && 
+    !name.includes(' ')        
+  );
 }
-
 function sayHello() {
   let userName = prompt('Введите ваше имя');
 

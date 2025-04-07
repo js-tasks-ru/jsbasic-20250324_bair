9f8663c9d34d0f2ff5e2881aff5fad931328cfba
unknown <botoshkin.b@gmail.com>
2025-04-07 14:33:19 +0300

    module 3

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,11 +1,3 @@
 function isEmpty(obj) {
   return Object.keys(obj).length === 0;
 }
-
-let schedule = {};
-
-alert( isEmpty(schedule) ); 
-
-schedule["8:30"] = "подъём";
-
-alert( isEmpty(schedule) ); 
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,16 @@
 function getMinMax(str) {
-  // ваш код...
+  const parts = str.split(' ');
+  const numbers = [];
+
+  for (const part of parts) {
+    const number = parseFloat(part);
+    if (!isNaN(number)) {
+      numbers.push(number);
+    }
+  }
+
+  const min = Math.min(...numbers);
+  const max = Math.max(...numbers);
+
+  return { min, max };
 }
